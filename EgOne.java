6813df880283f4f91349e7f36c3a456bd8c46532
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-03-19 14:52:52 +0545

    6

---
+++ EgOne.java
@@ -0,0 +1,11 @@
+package Practical;
+import mypack.Circle;
+
+public class EgOne {
+	    public static void main(String a[])  
+	    {      
+	    	Circle c = new Circle();   
+	    	c.area(2);   
+	    	}
+	    } 
+
---
+++ mypack/Circle.java
@@ -0,0 +1,11 @@
+package mypack; 
+
+public class Circle {
+	
+	    double r; 
+	   public void area(int a) 
+	    {    r=a;
+	    	System.out.println("Area of the circle = " + (3.14 * r * r));  
+	    	} 
+	   
+	    } 
